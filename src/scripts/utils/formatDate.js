419aebfcecd39a1d5d157073601b3226011410f4
Klochkov Aleksandr <sajkeekloch@gmail.com>
2020-03-14 21:57:07 +0300

    JS after review

--- src/scripts/utils/formatDate.js
+++ src/scripts/utils/formatDate.js
@@ -1,23 +1,3 @@
-class Counter {
-  add() {
-    if (!(localStorage.getItem('n'))) {
-      localStorage.setItem('n', 3);
-    } else {
-      localStorage.setItem('n', parseInt(localStorage.getItem('n')) + 3);
-    }
-  }
-
-  setTotal(t) {
-    localStorage.setItem('t', t);
-  }
-
-  reset() {
-    localStorage.setItem('n', 0);
-  }
-}
-
-export default Counter;
-
 function formatDate(date) {
   const monthNames = [
     'Января', 'Февраля', 'Марта',
